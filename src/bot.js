0764a78812409d274b75b31aaa171bd273714553
Jazem <jazemf.s@gmail.com>
2020-12-26 18:55:32 -0800

    add message event callback

--- src/bot.js
+++ src/bot.js
@@ -6,5 +6,12 @@ const client = new Client();
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
 });
+
+client.on('message', (message) => {
+    console.log(`[${message.author.tag}] ${message.content}`);
+    if (message.content === 'hello') {
+        message.reply("bye.");
+    }
+});
  
 client.login(process.env.DISCORD_BOT_TOKEN);
