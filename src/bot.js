280f327d61290529988bc00d7ae916e59089a426
Jazem <jazemf.s@gmail.com>
2020-12-31 21:33:14 -0800

    add args support

--- src/bot.js
+++ src/bot.js
@@ -10,34 +10,36 @@ client.on('ready', () => {
 client.on('message', (message) => {
     console.log(`[${message.author.tag}]: ${message.content}`); 
 
-    if (message.content.startsWith(prefix)) {
-        if (message.content.startsWith('ping', 1)) {
-            message.reply(`latency = ${Date.now() - message.createdTimestamp}ms`);
-        } 
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
 
-        else if (message.content.startsWith('coinflip', 1)) {
-            var outcome = Math.floor(Math.random() * 2);
-            if (outcome === 0) {
-                message.reply(`Heads!`);
-            } else {
-                message.reply(`Tails!`);
-            }
-        }
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-        else if (message.content.startsWith('help', 1)) {
-            message.channel.send({embed: {
-                color: 3447003,
-                description: "A very simple Embed!"
-              }});
-        }
-        
-        else {
-            message.reply(`command ${message.content} is not recognized.`);
+    if (args.length === 0) {
+        message.reply(`You have not input any arguments. Type ${prefix}help for a list of commands.`);
+    }
+
+    if (message.content.startsWith('ping', 1)) {
+        message.reply(`latency = ${Date.now() - message.createdTimestamp}ms`);
+    }
+
+    else if (message.content.startsWith('coinflip', 1)) {
+        var outcome = Math.floor(Math.random() * 2);
+        if (outcome === 0) {
+            message.reply(`Heads!`);
+        } else {
+            message.reply(`Tails!`);
         }
     }
 
-    if (message.content === 'hello' || message.content === 'hi') {
-        message.reply('bye.');
+    else if (message.content.startsWith('help', 1)) {
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "A very simple Embed!"
+            }});
     }
 });
  
